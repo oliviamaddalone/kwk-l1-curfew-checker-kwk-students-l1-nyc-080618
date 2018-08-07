c179727c3236b98f503ef1d519596809436333c0
oliviamaddalone <livmaddalone@gmail.com>
2018-08-07 20:05:32 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,14 +1,15 @@
-def simple_curfew_checker(time)
-  # code goes here
-end
 
-def curfew_checker(time)
-  # code goes here
-end
+def simple_curfew_checker
+  
+if time== 11
+  puts "Time to apparate"
 
-def complex_curfew_checker(time)
-  # code goes here
-end
+elsif time>11
+
+  puts "You're in trouble! Better get home quick"
+  
+else
+  time=11-time
 
 def deluxe_curfew_checker(time)
   # code goes here
